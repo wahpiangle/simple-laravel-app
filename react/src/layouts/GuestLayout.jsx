40294769ca6fd351a733beb17891b14e2f495191
quanming <qmtan17@gmail.com>
2023-08-31 00:09:24 +0800

    Update GuestLayout.jsx

--- react/src/layouts/GuestLayout.jsx
+++ react/src/layouts/GuestLayout.jsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
+import useSearchContext from "../context/ContextProvider"
+import { Navigate } from "react-router-dom";
 
 export default function GuestLayout() {
+	const { token } = useSearchContext();
+	if (token) {
+		return <Navigate to="/" />
+	}
 	return (
 		<div>
-				<div>
-						<Outlet />
-				</div>
+			<div>
+				<Outlet />
+			</div>
 		</div>
 	);
 }
